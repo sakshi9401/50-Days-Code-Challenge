66ff95833932e7318128bfd3e2b507dcb4ebb5f0
sakshi raikwar <68016527+sakshi9401@users.noreply.github.com>
2022-10-07 22:58:50 +0530

    Update factorial_variations.py

--- Day 5/factorial_variations.py
+++ Day 5/factorial_variations.py
@@ -1,13 +1,50 @@
+# problem of Factorial withod using any function
+
+num = int(input("Enter a number") 
+fact = 1
+if (num < 0):
+    num1 = (- num) 
+    for i in range(num1): 
+            fact = fact (i+1) 
+    print(f"Factorial of {-num1} is {-fact}") 
+
+elif (num or num == 1): 
+    print(f"Factorial of {num} is 1")
+
+elif (num> 0):
+    for i in range(num):
+        fact fact (i+1)
+    print("Factorial of {num} is {fact}")
+
+else:
+    print("No Factorial")
+
+          
+
+# performing Factorial problem using Function
+
+def factorial (num): 
+    fact = 1
+    if num < 0:
+        num1 = (-num)
+        for i in range(num1):
+            fact fact (i+1)
+        print(f"factorial of {num} is -{fact})
+
+    elif num >0: 
+        for i in range(num):
+            fact fact (i+1)
+        print (f"factorial of {num} is {fact})
+   
+    elif num == 8 or num == 1:
+        print(f"factorial of {num} is 1")
+    
+    else:
+        print("you entered invalid syntax")
 
 
-
-
-
-
-
-
-
-
+num = int(input("Enter a number: "))
+f = factorial (num)
 
 
 
