cc56ce82be65d7c1a3ab591eec96436b3a3917dc
sakshi raikwar <68016527+sakshi9401@users.noreply.github.com>
2022-11-01 18:33:42 +0530

    Create finding the percentage.py

---
+++ Day 8/finding the percentage.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    sum = 0
+    if query_name in student_marks:
+        for i in student_marks[query_name]:
+            sum = sum + i
+        print("{0:.2f}".format(sum/3))       
