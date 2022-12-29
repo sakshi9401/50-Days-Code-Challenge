f7cf91d4de73d5e68ba9c4e80f057972bd69bbe2
sakshi raikwar <68016527+sakshi9401@users.noreply.github.com>
2022-12-29 11:24:14 +0530

    Update Selection Sort.py

--- Day 23/Selection Sort.py
+++ Day 23/Selection Sort.py
@@ -1,1 +1,22 @@
+def selectionSort(array, size):
+	
+	for s in range(size):
+		  minimum = s
+	    for i in range(s + 1, size):
+		      if array[i] < array[minimum]:
+				  minimum = i
+		  (array[s], array[minimum]) = (array[minimum], array[s])
+
+arr = []
+n = int(input("Enter number of elements : "))
+
+for i in range(0, n):
+    ele = int(input())
+    arr.append(ele)
+
+size = len(arr)
+selectionSort(arr, size)
+
+print('Sorted Array in Ascending Order is :')
+print(arr)
 
