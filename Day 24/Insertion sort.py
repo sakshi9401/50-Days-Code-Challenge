a477b98717a7d507143d2b9ddcd94bef2d52e400
sakshi raikwar <68016527+sakshi9401@users.noreply.github.com>
2022-12-29 11:16:40 +0530

    Update Insertion sort.py

--- Day 24/Insertion sort.py
+++ Day 24/Insertion sort.py
@@ -1,1 +1,22 @@
+def insertion_sort(arr):  
+    for i in range(1, len(arr)):  
+  
+        value = arr[i]  
+  
+        j = i - 1  
+        while j >= 0 and value < arr[j]:  
+            arr[j + 1] = arr[j]  
+            j -= 1  
+            arr[j + 1] = value  
+    return arr  
+            
+try:
+    arr = []
+      
+    while True:
+        arr.append(int(input()))
 
+except:
+    print("The unsorted list is:", arr)  
+  
+    print("The sorted list1 is:", insertion_sort(arr)) 
