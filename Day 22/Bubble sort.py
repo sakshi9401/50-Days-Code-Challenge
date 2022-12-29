ed4f49c08a89c2f35c8e2970fe96e111832577c9
sakshi raikwar <68016527+sakshi9401@users.noreply.github.com>
2022-12-29 11:20:38 +0530

    Update Bubble sort.py

--- Day 22/Bubble sort.py
+++ Day 22/Bubble sort.py
@@ -1,1 +1,25 @@
+def bubbleSort(arr):
+	
+	n = len(arr)
+	
+	for i in range(n):
+		for j in range(0, n - i - 1):
+		
+			if arr[j] > arr[j + 1]:
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+				
+arr = []
+
+n = int(input("Enter number of elements : "))
+
+for i in range(0, n):
+    ele = int(input())
+  
+    arr.append(ele)
+
+bubbleSort(arr)
+
+print("Sorted array is:")
+for i in range(len(arr)):
+	print("%d" % arr[i])
 
